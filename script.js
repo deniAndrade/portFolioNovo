523efdbf0de21ab76564a943740c6093ce01a76e
deniAndrade <100054225+deniAndrade@users.noreply.github.com>
2022-03-10 20:13:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+var hamberguer = document.querySelector('.hamberguer')
+
+hamberguer.addEventListener('click', function () {
+  document.querySelector('.conteiner').classList.toggle('show-menu')
+})
+
+document.querySelector('#qtde').addEventListener('change', atualizarPreco)
+document.querySelector('#js' ).addEventListener('change', atualizarPreco)
+document.querySelector('#layout-sim').addEventListener('change', atualizarPreco)
+document.querySelector('#layout-nao').addEventListener('change', atualizarPreco)
+
+document.querySelector('#prazo').addEventListener('change', 
+function (){
+  const prazo = document.querySelector("#prazo").value
+  document.querySelector('label[for=prazo]').innerHTML =`prazo: ${prazo} semanas`
+ atualizarPreco()
+})
+
+function atualizarPreco() {
+  const qtde = document.querySelector('#qtde').value
+  const temJS = document.querySelector("#js" ).checked
+  const incruiLayount = document.querySelector("#layout-sim").checked
+  const prazo = document.querySelector("#prazo").value
+  let preco = qtde * 100
+  if (temJS) {
+    preco = preco + (preco * 10) / 100
+  }
+  if ( incruiLayount){
+    preco = preco + 500
+  }
+  let taxaUrgencia = 1 - prazo * 0.1;
+  preco *= 1 + taxaUrgencia
+  document.querySelector('#preco').innerHTML = `R$ ${preco.toFixed(2)}`
+}
+
